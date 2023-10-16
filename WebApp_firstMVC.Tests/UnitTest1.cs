308d8fd701ba4e0813c2b4c06383d8d04291140c
Sergio Azcona <SergioAzcona@odysseylogistics.com>
2023-10-16 16:51:26 -0400

    test created

---
+++ WebApp_firstMVC.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApp_firstMVC.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WebApplication_firstMVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Details(int Id)
+        {
+            return View("Details");
+        }
+    }
+}
