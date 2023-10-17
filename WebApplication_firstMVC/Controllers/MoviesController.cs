aabf03b7203bc294c3c06240a984d29239117d3e
Sergio Azcona <SergioAzcona@odysseylogistics.com>
2023-10-17 15:32:22 -0400

    removed logger from Movies branch

--- WebApplication_firstMVC/Controllers/MoviesController.cs
+++ WebApplication_firstMVC/Controllers/MoviesController.cs
@@ -6,13 +6,6 @@ namespace WebApplication_firstMVC.Controllers
 {
     public class MoviesController : Controller
     {
-        private readonly ILogger<MoviesController> _logger;
-
-        public MoviesController(ILogger<MoviesController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
