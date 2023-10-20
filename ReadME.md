ff3741196331f8c8ea346243dd937cc58875e26a
Sergio <sergioazcona@odysseylogistics.com>
2023-10-20 09:55:14 -0400

    ReadMe updated
    change to RM

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+Hello!
+This project was built using the ASP.NET Core MVC tutorial found on this site: 
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-7.0&tabs=visual-studio
+
+Additional resource: https://launch.turing.edu/module1/
---
+++ WebApplication_firstMVC/Data/WebApplication_firstMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication_firstMVC.Models;
+
+namespace WebApplication_firstMVC.Data
+{
+    public class WebApplication_firstMVCContext : DbContext
+    {
+        public WebApplication_firstMVCContext (DbContextOptions<WebApplication_firstMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication_firstMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
