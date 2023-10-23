fa7d560d1305c1f6942ceed0f2da8d4a9f3f39e8
Sergio <sergioazcona@odysseylogistics.com>
2023-10-23 09:54:06 -0400

    Added client controller and view (basics); minor changes to landing page

---
+++ WebApplication_firstMVC/Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_firstMVC.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
